3faa556a8b4270040da2ca7aecd44d49ada43d28
ChaikaBogdan <gs.chaikabogdan@gmail.com>
2024-06-24 15:43:36 +0300

    fix

--- converter.py
+++ converter.py
@@ -4,7 +4,7 @@ import tempfile
 import math
 import os
 from PIL import Image
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +23,9 @@ def _get_number_rounded_up_to_even(number: int) -> int:
     return int(math.ceil(float(number) / 2.0) * 2.0)
 
 
-def _get_resized_clip_dimensions(clip_width: int, clip_height: int, max_size: int = 1280) -> tuple[int, int]:
+def _get_resized_clip_dimensions(
+    clip_width: int, clip_height: int, max_size: int = 1280
+) -> tuple[int, int]:
     if _get_number_rounded_up_to_even(max_size) != max_size:
         raise ValueError("Clip resize max size should be number divisible by two")
     aspect_ratio = float(clip_width) / float(clip_height)
@@ -48,13 +50,24 @@ def _convert2MP4(filename: str) -> str:
     temp_audio_filename = _get_converted_name("m4a")
     clip = VideoFileClip(filename)
     clip_width, clip_height = clip.size
-    new_clip_width, new_clip_height = _get_resized_clip_dimensions(clip_width, clip_height)
+    fps = clip.fps
+    if fps < 30:
+        fps = 30
+    duration = clip.duration
+    if duration <= 1.0:
+        # Loop the clip until its total duration is at least 5 second
+        loop_count = int(5.0 / duration) + 1
+        clips = [clip] * loop_count
+        video_clip = concatenate_videoclips(clips, method="chain").subclip(0, 5.0)
+    else:
+        video_clip = clip
+    new_clip_width, new_clip_height = _get_resized_clip_dimensions(
+        clip_width, clip_height
+    )
     resize_kwargs = {}
-    if new_clip_width != clip_width:
-       resize_kwargs['width'] = new_clip_width
-    if new_clip_height != clip_height:
-       resize_kwargs['height'] = new_clip_height
-    ffmpeg_params=[
+    resize_kwargs["width"] = new_clip_width
+    resize_kwargs["height"] = new_clip_height
+    ffmpeg_params = [
         "-profile:v",
         "main",
         "-level",
@@ -64,15 +77,17 @@ def _convert2MP4(filename: str) -> str:
         "-crf",
         "29",
         "-b:v",
-        "500",
+        "500k",
         "-pix_fmt",
         "yuv420p",
+        "-r",
+        str(fps),
     ]
-    if resize_kwargs:
+    if new_clip_width != clip_width or new_clip_height != clip_height:
         ffmpeg_params.append("-vf")
         scale_param = f"scale={resize_kwargs['width']}:{resize_kwargs['height']}"
         ffmpeg_params.append(scale_param)
-    clip.write_videofile(
+    video_clip.write_videofile(
         converted_name,
         codec="libx264",
         ffmpeg_params=ffmpeg_params,
@@ -83,14 +98,14 @@ def _convert2MP4(filename: str) -> str:
         preset="slow",  # Preset for encoding speed vs. quality balance
         logger=None,
     )
-    clip.close()
+    video_clip.close()
     return converted_name
 
 
 async def convert2MP4(filename: str) -> str:
-   loop = asyncio.get_event_loop()
-   converted_file_name = await loop.run_in_executor(None, _convert2MP4, filename)
-   return converted_file_name
+    loop = asyncio.get_event_loop()
+    converted_file_name = await loop.run_in_executor(None, _convert2MP4, filename)
+    return converted_file_name
 
 
 def _convert2JPG(filename: str) -> str:
@@ -101,6 +116,6 @@ def _convert2JPG(filename: str) -> str:
 
 
 async def convert2JPG(filename: str) -> str:
-   loop = asyncio.get_event_loop()
-   converted_file_name = await loop.run_in_executor(None, _convert2JPG, filename)
-   return converted_file_name
+    loop = asyncio.get_event_loop()
+    converted_file_name = await loop.run_in_executor(None, _convert2JPG, filename)
+    return converted_file_name
